795038b791c588b703450789870f4941af0d3731
Rupesh agrawal <rupesh.agrawal1904@outlook.com>
2023-06-14 13:02:39 +0530

    Connected To Database

---
+++ 01-rest-api-starting-code/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into Todo(id,description,done, target_Date,username)
+values(1001,'learn Aws',false,CURRENT_DATE(),'in28minutes') ;
+
+insert into Todo(id,description,done, target_Date,username)
+values(1002,'learn Azure',false,CURRENT_DATE(),'in28minutes') ;
+
+insert into Todo(id,description,done, target_Date,username)
+values(1003,'learn fullstack',false,CURRENT_DATE(),'in28minutes') ;
